50be548e03da26ffe9ea3c34215292fd948973f0
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-06-10 17:23:54 +0200

    menu nach kategroien, lässt sich noch nicht anzeigen

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,8 @@ from menu import views
 
 urlpatterns = [
     
-    path("", views.menu_list, name="menu"),
+    #path("", views.drinks_list, name="drinks"),
+    #path("", views.starters_list, name="starters"),
+    #path("", views.main_dishes_list, name="main_dishes"),
+    path("", views.desserts_list, name="desserts"),
 ]
